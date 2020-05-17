18c16a87a98b9e2ed5a9ce97924891f6115e945a
Anastey6a <61656958+Anastey6a@users.noreply.github.com>
2020-05-17 17:14:29 +0300

    nastya

--- script.js
+++ script.js
@@ -1,53 +1,15 @@
 $("document").ready(function(){
-	$(".box4-simple-button").click(function(){
-		let x = this.id;
-		if ( x == "box4-simple-button1") {
-			document.getElementById("box4-more1").style.display = "block";
-			document.getElementById("box4-more2").style.display = "none";
-			document.getElementById("box4-more3").style.display = "none";
-			document.getElementById("box4-simple-content1").style.display = "block";
-			document.getElementById("box4-simple-content2").style.display = "none";
-			document.getElementById("box4-simple-content3").style.display = "none";
-			document.getElementById("box4-simple-button1").style.backgroundColor = "#c99077";
-			document.getElementById("box4-simple-button2").style.backgroundColor = "#e8a487";
-			document.getElementById("box4-simple-button3").style.backgroundColor = "#e8a487";
-
-			document.getElementById("box4-simple-button1").style.borderBottom = "solid #c9a3d1 0px";
-			document.getElementById("box4-simple-button2").style.borderBottom = "solid #c9a3d1 2px";
-			document.getElementById("box4-simple-button3").style.borderBottom = "solid #c9a3d1 2px";
+		let s = 1;
+	setInterval(function(){
+		if (s == 1) {
+			document.getElementById("box1-element1").style.display = "none";
+			document.getElementById("box1-element2").style.display = "block";
+			s = 2;
 		}
-		else if(x == "box4-simple-button2") {
-			document.getElementById("box4-more1").style.display = "none";
-			document.getElementById("box4-more2").style.display = "block";
-			document.getElementById("box4-more3").style.display = "none";
-			document.getElementById("box4-simple-content1").style.display = "none";
-			document.getElementById("box4-simple-content2").style.display = "block";
-			document.getElementById("box4-simple-content3").style.display = "none";
-			document.getElementById("box4-simple-button1").style.backgroundColor = "#e8a487";
-			document.getElementById("box4-simple-button2").style.backgroundColor = "#c99077";
-			document.getElementById("box4-simple-button3").style.backgroundColor = "#e8a487";
-
-			document.getElementById("box4-simple-button1").style.borderBottom = "solid #c9a3d1 2px";
-			document.getElementById("box4-simple-button2").style.borderBottom = "solid #c9a3d1 0px";
-			document.getElementById("box4-simple-button3").style.borderBottom = "solid #c9a3d1 2px";
-			
+		else{
+			document.getElementById("box1-element2").style.display = "none";
+			document.getElementById("box1-element1").style.display = "block";
+			s = 1;
 		}
-		else if(x == "box4-simple-button3") {
-			document.getElementById("box4-more1").style.display = "none";
-			document.getElementById("box4-more2").style.display = "none";
-			document.getElementById("box4-more3").style.display = "block";
-			document.getElementById("box4-simple-content1").style.display = "none";
-			document.getElementById("box4-simple-content2").style.display = "none";
-			document.getElementById("box4-simple-content3").style.display = "block";
-
-			document.getElementById("box4-simple-button1").style.backgroundColor = "#e8a487";
-			document.getElementById("box4-simple-button2").style.backgroundColor = "#e8a487";
-			document.getElementById("box4-simple-button3").style.backgroundColor = "#c99077";
-
-
-			document.getElementById("box4-simple-button1").style.borderBottom = "solid #c9a3d1 2px";
-			document.getElementById("box4-simple-button2").style.borderBottom = "solid #c9a3d1 2px";
-			document.getElementById("box4-simple-button3").style.borderBottom = "solid #c9a3d1 0px";
-		}
-	})
+	}, 3500);
 })
